c09458f5fb8e441e5caceb154b0a304ab62e5e42
awahler <78176344+awahler@users.noreply.github.com>
2021-05-12 12:31:32 -0400

    Add files via upload

---
+++ Final Project/IMU_Classifer.ino
@@ -0,0 +1,121 @@
+#include <Arduino_LSM9DS1.h>
+
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/micro/all_ops_resolver.h>
+#include <tensorflow/lite/micro/micro_error_reporter.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/version.h>
+
+#include "model.h"
+
+const float accelerationThreshold = 2.5; 
+const int numSamples = 119;
+
+int samplesRead = numSamples;
+
+tflite::MicroErrorReporter tflErrorReporter;
+
+tflite::AllOpsResolver tflOpsResolver;
+
+const tflite::Model* tflModel = nullptr;
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize];
+
+const char* GESTURES[] = {
+  "punch",
+  "flex"
+};
+
+#define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+
+  Serial.println();
+
+  tflModel = tflite::GetModel(model);
+  if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
+    Serial.println("Model schema mismatch!");
+    while (1);
+  }
+
+  tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
+
+  tflInterpreter->AllocateTensors();
+
+  tflInputTensor = tflInterpreter->input(0);
+  tflOutputTensor = tflInterpreter->output(0);
+}
+
+void loop() {
+  float aX, aY, aZ, gX, gY, gZ;
+  while (samplesRead == numSamples) {
+    if (IMU.accelerationAvailable()) {
+      
+      IMU.readAcceleration(aX, aY, aZ);
+
+      float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+
+      if (aSum >= accelerationThreshold) {
+       
+        samplesRead = 0;
+        break;
+      }
+    }
+  }
+
+  
+  while (samplesRead < numSamples) {
+  
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+      
+      IMU.readAcceleration(aX, aY, aZ);
+      IMU.readGyroscope(gX, gY, gZ);
+
+      tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 3] = (gX + 2000.0) / 4000.0;
+      tflInputTensor->data.f[samplesRead * 6 + 4] = (gY + 2000.0) / 4000.0;
+      tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
+
+      samplesRead++;
+
+      if (samplesRead == numSamples) {
+       
+        TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+        if (invokeStatus != kTfLiteOk) {
+          Serial.println("Invoke failed!");
+          while (1);
+          return;
+        }
+
+        
+        for (int i = 0; i < NUM_GESTURES; i++) {
+          Serial.print(GESTURES[i]);
+          Serial.print(": ");
+          Serial.println(tflOutputTensor->data.f[i], 6);
+        }
+        Serial.println();
+      }
+    }
+  }
+}
